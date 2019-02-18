ecefa5382ed09c4328ac16a44183f1a22ef7e615
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2019-02-19 00:32:24 +0530

    Add program documentation + convert document to run as a module + script

--- linear_search_day1.py
+++ linear_search_day1.py
@@ -1,3 +1,12 @@
+"""
+This program is a generalized program for linear search.
+The generalization is on taking user input even for the list of values, also asking for the input of the search element.
+
+This code can be even more generalized by checking condition if we had to take input from the users or from the system
+input. But it's not implemented here.
+"""
+
+
 # Function definition of linear search algorithm. The function takes in three parameters
 # 1. The input list
 # 2. The search element value
@@ -14,12 +23,14 @@ def linear_search(input_list, search_element, length):
     # If in the for loop, none of the values in the list match the search element, then return -1
     return -1
 
-# Taking the input values as a complete string and using the map method, convert the string into a map function and type cast it to return a list of integer values.
-input_list = list(map(float, input("Enter elements of the list seperated by a space: \n").split()))
-# Taking the input value of the search element.
-search_element = float(input("Enter the search element: "))
-# Calculating the length of the input list
-length = len(input_list)
 
-# Printing the output state of the returned value from the function.
-print(linear_search(input_list=input_list, search_element=search_element, length=length))
+if __name__ = "__main__":
+    # Taking the input values as a complete string and using the map method, convert the string into a map function and type cast it to return a list of integer values.
+    input_list = list(map(float, input("Enter elements of the list seperated by a space: \n").split()))
+    # Taking the input value of the search element.
+    search_element = float(input("Enter the search element: "))
+    # Calculating the length of the input list
+    length = len(input_list)
+
+    # Printing the output state of the returned value from the function.
+    print(linear_search(input_list=input_list, search_element=search_element, length=length))
